7469c798db14e7d5aff3b3985bde534058af8f70
Ben Hall <rcmast3r1@gmail.com>
2022-08-18 19:52:17 -0400

    removed thing that didnt exist

--- install_scripts/installAptDeps_humble.sh
+++ install_scripts/installAptDeps_humble.sh
@@ -4,7 +4,6 @@ set -e
 
 sudo apt install -y python3-vcstool python3-pip python3-rosdep python3-colcon-common-extensions ros-humble-gazebo-ros-pkgs ros-humble-xacro git nano
 sudo apt install -y ros-humble-ackermann-msgs
-sudo apt install -y ros-humble-pcl
 sudo apt install -y ros-humble-pcl-ros
 sudo apt install -y ros-humble-diagnostic-updater
 sudo apt install -y libopencv-dev python3-opencv
